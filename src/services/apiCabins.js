ffcf8418ebdd51ade9f33841fc91ba79c1087378
Naman Yadav <raymannky2001@gmail.com>
2023-09-28 14:24:31 +0530

    start working on forms using ReacthookForms

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,11 +1,21 @@
 import supabase from "./supabase";
 
-async function getCabins() {
+export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.error(error);
     throw new Error("getting problem in fetching error");
+  }
+  return data;
+}
+
+export async function deleteCabin(id) {
+  let { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("getting problem in deleting Cabin");
   }
   return data;
 }
